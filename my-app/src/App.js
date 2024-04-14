0e06ec9ab72980163ae191a8b1f0455a5548b675
maulshreegarg <98210535+maulshreegarg@users.noreply.github.com>
2024-04-14 23:13:26 +0530

    compoenent

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
    
    <>
-   <nav>
-    <li>home</li>
-    <li>train</li>
-    <li>file</li>
-   </nav>
-   <div className="a">dewfewrfrf rfr frew fr fer fre fer tgtr grt hyrh tu uju juty hyh trgte ser</div>
+   <Navbar/>
    </>
   );
 }
